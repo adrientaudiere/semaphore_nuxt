a2a209938dd4cdbc5ec405c8c8a062233d4c7592
adrientaudiere <adrien@LeGarsBelou>
2021-01-21 16:24:10 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Quicksand', 'sans-serif'],
+      serif: ['Georgia', 'serif']
+    },
+    colors: {
+      gris: '#454839',
+      bleu: '#0097ae',
+      orange: '#df5e00',
+      vert: '#bada55',
+      rouge: '#d72323',
+      gris_clair: '#aaad9b',
+      cyan: '#00cfe6'
+    }
+  }
+}
