3e28f6defbc2354d73e7f4ab6b08d4bd6591f666
Adrien Taudiere <adrien.taudiere@tutanota.com>
2021-03-15 16:40:09 +0100

    remove unimportant fork awesome files

--- pages/formation.vue
+++ pages/formation.vue
@@ -3,7 +3,6 @@
     <div class="w-screen h-full ">
       <div class="my-12 text-justify px-4 max-w-sm md:max-w-2xl m-auto">
         <h1>{{ page.title }}</h1>
-        {{ page.description }}
         <nuxt-content :document="page" />
       </div>
     </div>
